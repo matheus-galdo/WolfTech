1a110c11c03c59b9cca8e398781e285b49e3ad38
Matheus Galdino <matheus.kho@gmail.com>
2023-05-11 00:52:57 -0300

    refactor: adding authService by dependency injection

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,76 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\DataObjects\UserDataObject;
-use App\Exceptions\TesteException;
-use App\Http\Middleware\ErrorHandlerMiddleware;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Service\AuthService;
-use Closure;
-use Exception;
 use Illuminate\Http\JsonResponse;
-use Throwable;
-
-class ErroLogin extends Exception
-{
-    
-}
-
 
 class AuthController extends Controller
 {
-    public function __construct()
+    public function __construct(public AuthService $authService)
     {
         $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
     public function login(LoginRequest $request)
     {
-        try {
-            $credentials = $request->only('email', 'password');
-            $response = AuthService::login($credentials);
-            return response()->json($response);
-        } catch (Throwable $th) {
-            throw $th;
-        }
+        $credentials = $request->only('email', 'password');
+        $response = $this->authService->login($credentials);
+        return response()->json($response);
     }
 
 
-    public function register(RegisterRequest $request, Closure $next): JsonResponse
+    public function register(RegisterRequest $request): JsonResponse
     {
-        try {
-            $requestCredentials = new UserDataObject(
-                id: null,
-                name: $request->input('name'),
-                email: $request->input('email'),
-                password: $request->input('email'),
-            );
+        $requestCredentials = new UserDataObject(
+            id: null,
+            name: $request->input('name'),
+            email: $request->input('email'),
+            password: $request->input('email'),
+        );
 
-            $response = AuthService::register($requestCredentials);
-            return response()->json(status: 201, data: $response);
-        } catch (\Throwable $th) {
-            return $th;
-        }
+        $response = $this->authService->register($requestCredentials);
+        return response()->json(status: 201, data: $response);
     }
 
     public function logout()
     {
-        try {
-            AuthService::logout();
-            return response()->json(['message' => 'Successfully logged out'], 200);
-        } catch (\Throwable $th) {
-            //throw $th;
-            return response()->json(["message" => $th->getMessage()], 401);
-        }
+        $this->authService->logout();
+        return response()->json(['message' => 'Successfully logged out'], 200);
     }
 
     public function refresh()
     {
-        try {
-            $response = AuthService::refresh();
-            return response()->json($response, 201);
-        } catch (\Throwable $th) {
-            return response()->json(["message" => $th->getMessage()], 400);
-        }
+        $response = $this->authService->refresh();
+        return response()->json($response, 201);
     }
 }
