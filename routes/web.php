a50327addd90bebc7e62f8cf783f5f53f191f20d
Matheus Galdino <matheus.kho@gmail.com>
2023-05-13 18:27:11 -0300

    refactor: remove unused blade files and cleaning api routes file

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('loja/home');
-});
-
-Route::get('produto/detalhes', function () {
-    return view('loja/produto', [
-        'nomeProduto' => 'Processador i5 8400'
-    ]);
-});
-
-Route::get('buscar/', function ($pesquisar) {
-    echo $pesquisar;
+    return redirect('/api');
 });
